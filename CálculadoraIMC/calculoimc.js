34792717d34b5a1c76a0260a0a70d5d2b43fd533
DanTinho <161615116+DanTinho@users.noreply.github.com>
2024-10-17 11:39:37 -0300

    Add files via upload

--- CálculadoraIMC/calculoimc.js
+++ CálculadoraIMC/calculoimc.js
@@ -1,24 +1,19 @@
 function perguntarPeso() {
-    
-    let peso = 0;
-    let altura = 0;
 
     peso = prompt("Qual seu peso?");
     altura = prompt("Qual sua altura em metros?");
 
-    imc1 = altura * altura
-    imc2 = peso / imc1
+    let imc = peso / (altura * altura);
 
     let resposta = window.document.getElementById('valorImc');
 
-    if (imc2 < 18,5) {
+    if(imc < 18.5) {
     resposta.innerHTML += "<p>Considerando seu peso de " + peso + "kg e sua altura de " + altura + "m, você está abaixo do peso.<p>";
-    } else if (18,51 < imc2 > 24,99) {
+    } else if (imc > 18.6 && imc < 24.9) {
     resposta.innerHTML += "<p>Considerando seu peso de " + peso + "kg e sua altura de " + altura + "m, você está no peso normal.<p>";
-    } else if (25 < imc2 > 29,99) {
+    } else if (imc > 25 && imc < 29.9) {
     resposta.innerHTML += "<p>Considerando seu peso de " + peso + "kg e sua altura de " + altura + "m, você está acima do peso.<p>";
-    } else if (imc2 < 30) {
-    resposta.innerHTML += "<p>Considerando seu peso de " + peso + "kg e sua altura de " + altura + "m, você é obeso.<p>";
+    } else if (imc > 30) {
+    resposta.innerHTML += "<p>Considerando seu peso de " + peso + "kg e sua altura de " + altura + "m, você está obeso.<p>";
     }
-
 }
